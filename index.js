4eaf194ce8a2313f429d23d21437a52bfffd7a96
Nicollie Machtura Goes <111029564+nicolliegoes@users.noreply.github.com>
2023-04-05 15:07:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function index(){
+window.location.href='index.html';
+}
+function Motivosdosataques(){
+    window.location.href='Motivosdosataques.html';
+}
+function Quaisosperigos(){
+    window.location.href='Quaisosperigos.html';
+}
+function Oqueeumespecialista(){
+    window.location.href='Oqueeumespecialista.html';
+}
+
+
